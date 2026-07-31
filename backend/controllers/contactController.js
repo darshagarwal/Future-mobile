ce912b55fb0ddf50412bc94a70db5123ee10aa77
darshagarwal <agarwaldarsh2007@gmail.com>
2026-07-31 19:08:44 +0530

    Switch Gmail SMTP from 465 to 587

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -10,10 +10,7 @@ const transporter = nodemailer.createTransport({
   },
   tls: {
     rejectUnauthorized: false
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+  }
 });
 
 exports.sendContactMail = async (req, res) => {
